02884e47ec1c29ac15b35113327a32b5a29f4729
Will Sturgeon <willstrgn@gmail.com>
2023-12-04 17:03:46 -0500

    Doesn't seem to work

--- theories/Context.v
+++ theories/Context.v
@@ -3,6 +3,7 @@ From Coq Require Import
   String.
 From LambdaST Require Import
   Ident
+  Invert
   Terms
   Types.
 
@@ -11,6 +12,14 @@ Inductive context : Set :=
   | CtxHasTy (id : ident) (ty : type)
   | CtxComma (lhs rhs : context)
   | CtxSemic (lhs rhs : context)
+  .
+
+Variant SameHead : context -> context -> Prop :=
+  | SameHeadId : forall ctx, SameHead ctx ctx
+  | SameHeadCommaL : forall ll lr r, SameHead (CtxComma ll r) (CtxComma lr r)
+  | SameHeadCommaR : forall l rl rr, SameHead (CtxComma l rl) (CtxComma l rr)
+  | SameHeadSemicL : forall ll lr r, SameHead (CtxSemic ll r) (CtxSemic lr r)
+  | SameHeadSemicR : forall l rl rr, SameHead (CtxSemic l rl) (CtxSemic l rr)
   .
 
 Inductive hole : Set :=
@@ -31,37 +40,6 @@ Fixpoint fill hole ctx :=
   end.
 
 Notation "G ( H )" := (fill G H) (at level 190).
-
-(*
-Inductive Contains : context -> context -> Prop :=
-  | ContainsExact : forall c,
-      Contains c c
-  | ContainsCommaL : forall c lhs rhs,
-      Contains c lhs ->
-      Contains c (CtxComma lhs rhs)
-  | ContainsCommaR : forall c lhs rhs,
-      Contains c rhs ->
-      Contains c (CtxComma lhs rhs)
-  | ContainsSemicL : forall c lhs rhs,
-      Contains c lhs ->
-      Contains c (CtxSemic lhs rhs)
-  | ContainsSemicR : forall c lhs rhs,
-      Contains c rhs ->
-      Contains c (CtxSemic lhs rhs)
-  .
-
-Ltac contains :=
-  intros;
-  try solve [apply ContainsExact];
-  try solve [apply ContainsCommaL; contains];
-  try solve [apply ContainsCommaR; contains];
-  try solve [apply ContainsSemicL; contains];
-  try solve [apply ContainsSemicR; contains];
-  fail.
-
-Example contains_semic_r : forall lhs rhs,
-  Contains rhs (CtxSemic lhs rhs).
-Proof. contains. Qed.
 
 Inductive FindReplace : context -> context -> context -> context -> Prop :=
   | FindReplExact : forall a b,
@@ -95,6 +73,37 @@ Proof.
   find_repl.
 Qed.
 
+(*
+Inductive Contains : context -> context -> Prop :=
+  | ContainsExact : forall c,
+      Contains c c
+  | ContainsCommaL : forall c lhs rhs,
+      Contains c lhs ->
+      Contains c (CtxComma lhs rhs)
+  | ContainsCommaR : forall c lhs rhs,
+      Contains c rhs ->
+      Contains c (CtxComma lhs rhs)
+  | ContainsSemicL : forall c lhs rhs,
+      Contains c lhs ->
+      Contains c (CtxSemic lhs rhs)
+  | ContainsSemicR : forall c lhs rhs,
+      Contains c rhs ->
+      Contains c (CtxSemic lhs rhs)
+  .
+
+Ltac contains :=
+  intros;
+  try solve [apply ContainsExact];
+  try solve [apply ContainsCommaL; contains];
+  try solve [apply ContainsCommaR; contains];
+  try solve [apply ContainsSemicL; contains];
+  try solve [apply ContainsSemicR; contains];
+  fail.
+
+Example contains_semic_r : forall lhs rhs,
+  Contains rhs (CtxSemic lhs rhs).
+Proof. contains. Qed.
+
 Theorem contains_is_find_repl_id : forall needle haystack,
   Contains needle haystack <-> FindReplace needle needle haystack haystack.
 Proof. split; intros; induction H; constructor; assumption. Qed.
@@ -106,3 +115,19 @@ Fixpoint vars_in ctx : list ident :=
   | CtxHasTy x _ => cons x nil
   | CtxComma lhs rhs | CtxSemic lhs rhs => vars_in lhs ++ vars_in rhs
   end.
+
+Theorem hole_to_find_repl : forall G A B,
+  FindReplace A B (fill G A) (fill G B).
+Proof. induction G; intros; constructor; apply IHG. Qed.
+
+Theorem hole_repr_eq : forall G A B GA GB,
+  (~SameHead A B) ->
+  ((fill G A = GA /\ fill G B = GB) <-> (FindReplace A B GA GB)).
+Proof.
+  induction G; intros.
+  - (* Hole at the root. *)
+    simpl. split.
+    + (* Fixpoint -> Inductive *) intros [HA HB]. invert HA. constructor.
+    + (* Inductive -> Fixpoint *) intros Hc. Print FindReplace. induction Hc.
+      * split; reflexivity.
+      * apply IHHc in H. invert H. split. fail.
