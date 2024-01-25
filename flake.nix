333c026e4b7e32924c17bbe4cffd52ba2a413fdc
Will Sturgeon <willstrgn@gmail.com>
2024-01-24 22:05:05 -0500

    Custom-build CoqHammer with Nix

--- flake.nix
+++ flake.nix
@@ -2,19 +2,27 @@
   description = "Operational semantics of general programs with streams.";
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    hammer-src = {
+      url = "github:lukaszcz/coqhammer";
+      flake = false;
+    };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = { flake-utils, nixpkgs, self }:
+  outputs = { flake-utils, hammer-src, nixpkgs, self }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pname = "lambda-st";
         version = "0.0.1";
         os-pkgs = import nixpkgs { inherit system; };
         pkgs = os-pkgs.coqPackages;
+        hammer = import ./hammer.nix {
+          inherit hammer-src nixpkgs system;
+        }; # build it manually
       in {
         packages.default = pkgs.mkCoqDerivation {
           inherit pname version;
           src = ./.;
+          buildInputs = [ hammer ];
         };
       });
 }
---
+++ hammer.nix
@@ -0,0 +1,46 @@
+inputs:
+with inputs;
+let
+  src = hammer-src;
+  pname = "hammer";
+  version = "none";
+  os-pkgs = import nixpkgs { inherit system; };
+  coq-pkgs = os-pkgs.coqPackages;
+  coq = coq-pkgs.coq;
+  ml-pkgs = coq.ocamlPackages;
+  ocaml = ml-pkgs.ocaml;
+  propagatedBuildInputs = [ coq ocaml ml-pkgs.findlib ];
+  mlPlugin = true;
+  BINDIR = "bin";
+  DESTDIR = "\${out}";
+  ml-suffix =
+    "/lib/ocaml/${ocaml.version}/site-lib"; # this is the magic incantation (<https://ryantm.github.io/nixpkgs/languages-frameworks/ocaml/#sec-language-ocaml-packaging>)
+  coq-suffix =
+    "/lib/coq/${coq.coq-version}/user-contrib"; # the other magic incantation, buried in the corresponding Coq page
+  COQLIBINSTALL = "\${out}${coq-suffix}";
+  COQDOCINSTALL = "\${out}/doc";
+  COQPLUGININSTALL = "\${out}${ml-suffix}";
+  COQTOPINSTALL = "\${out}/top";
+  tactics = coq-pkgs.mkCoqDerivation {
+    inherit propagatedBuildInputs mlPlugin src version BINDIR DESTDIR
+      COQLIBINSTALL COQDOCINSTALL COQPLUGININSTALL COQTOPINSTALL;
+    pname = "${pname}-tactics";
+    buildPhase = "make tactics";
+    installPhase = "make install-tactics";
+  };
+  plugin = coq-pkgs.mkCoqDerivation {
+    inherit mlPlugin src version BINDIR DESTDIR COQLIBINSTALL COQDOCINSTALL
+      COQPLUGININSTALL COQTOPINSTALL;
+    pname = "${pname}-plugin";
+    buildPhase = "make plugin";
+    installPhase = "make install-plugin";
+    propagatedBuildInputs = [ tactics ] ++ propagatedBuildInputs;
+  };
+in os-pkgs.stdenv.mkDerivation {
+  inherit pname src version;
+  phases = [ ];
+  buildPhase = ":"; # just in case
+  installPhase = ":"; # ^^
+  propagatedBuildInputs = [ plugin tactics ];
+}
+
