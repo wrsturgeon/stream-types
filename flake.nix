09d72f4e1534540dc5c3ba3bdc9d39ad50188bbc
Will Sturgeon <willstrgn@gmail.com>
2024-01-24 22:16:01 -0500

    Build CoqHammer with Nix in Joe's version (#6)
    * CI on push (#2)
    
    * CI on push
    
    * ...but don't duplicate the work
    
    * Write a README (#4)
    
    * Custom-build CoqHammer with Nix (#5)
    
    * Custom-build CoqHammer with Nix
    
    * Make CoqHammer dependencies more explicit

--- flake.nix
+++ flake.nix
@@ -2,19 +2,32 @@
   description = "Operational semantics of general programs with streams.";
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
+    hammer-src = {
+      url = "github:lukaszcz/coqhammer";
+      flake = false;
+    };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
-  outputs = { flake-utils, nixpkgs, self }:
+  outputs = { flake-utils, hammer-src, nixpkgs, self }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pname = "lambda-st";
         version = "0.0.1";
         os-pkgs = import nixpkgs { inherit system; };
-        pkgs = os-pkgs.coqPackages;
+        coq-pkgs = os-pkgs.coqPackages;
+        hammer = let
+          coq = coq-pkgs.coq;
+          ml-pkgs = coq.ocamlPackages; # guaranteed Coq-OCaml version match
+          ocaml = ml-pkgs.ocaml;
+        in import ./hammer.nix {
+          inherit coq coq-pkgs ml-pkgs ocaml os-pkgs;
+          src = hammer-src;
+        };
       in {
-        packages.default = pkgs.mkCoqDerivation {
+        packages.default = coq-pkgs.mkCoqDerivation {
           inherit pname version;
           src = ./.;
+          buildInputs = [ hammer ];
         };
       });
 }
