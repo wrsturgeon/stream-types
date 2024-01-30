2a55f075da2f507d128ce0eaa6eb6a649ecbd9cf
Will Sturgeon <willstrgn@gmail.com>
2024-01-30 00:05:36 -0500

    Update typing relation (#31)
    * Update typing relation
    
    * Use `Prop` version of `fill`

--- theories/Typing.v
+++ theories/Typing.v
@@ -1,5 +1,7 @@
 From LambdaST Require Import
   Context
+  FV
+  Hole
   Terms
   Types.
 
@@ -7,40 +9,76 @@ Declare Scope typing_scope.
 
 Reserved Notation "G '|-' x '\in' T" (at level 97).
 
-(*
 Inductive Typed : context -> term -> type -> Prop :=
-  | T_Par_R : forall G e1 e2 s t,
+  | TParR : forall G e1 e2 s t,
       G |- e1 \in s ->
       G |- e2 \in t ->
-      G |- (TmComma e1 e2) \in (TyPar s t)
-  | T_Par_L : forall Gxy Gz x y z s t e r,
-      FindReplace (CtxComma (CtxHasTy x s) (CtxHasTy y t)) (CtxHasTy z (TyPar s t)) Gxy Gz ->
-      Gxy |- e \in r ->
-      Gz |- (TmLetPar x y z e) \in r
-  | T_Cat_R : forall G D e1 e2 s t,
+      G |- (e1, e2) \in TyPar s t
+  | T_Par_L : forall G x y z s t e r Gxsyt Gzst,
+      x <> y ->
+      ~fv G x ->
+      ~fv G y ->
+      (*
+      fill G (CtxComma (CtxHasTy x s) (CtxHasTy y t)) |- e \in r ->
+      fill G (CtxHasTy z (TyPar s t)) |- (TmLetPar x y z e) \in r
+      *)
+      FillWith (CtxComma (CtxHasTy x s) (CtxHasTy y t)) G Gxsyt ->
+      FillWith (CtxHasTy z (TyPar s t)) G Gzst ->
+      Gxsyt |- e \in r ->
+      Gzst |- TmLetPar x y z e \in r
+  | TCatR : forall G D e1 e2 s t,
       G |- e1 \in s ->
       D |- e2 \in t ->
-      (CtxSemic G D) |- (e1; e2) \in (TyDot s t)
-  | T_Cat_L : forall Gxy Gz x y z s t e r,
-      FindReplace (CtxSemic (CtxHasTy x s) (CtxHasTy y t)) (CtxHasTy z (TyDot s t)) Gxy Gz ->
-      Gxy |- e \in r ->
-      Gz |- (TmLetCat x y z e) \in r
-  | T_Eps_R : forall G,
+      CtxSemic G D |- (e1; e2) \in TyDot s t
+  | T_Cat_L : forall G x y z s t e r Gxsyt Gzst,
+      x <> y ->
+      ~fv G x ->
+      ~fv G y ->
+      (*
+      fill G (CtxSemic (CtxHasTy x s) (CtxHasTy y t)) |- e \in r ->
+      fill G (CtxHasTy z (TyDot s t)) |- (TmLetCat t x y z e) \in r
+      *)
+      FillWith (CtxSemic (CtxHasTy x s) (CtxHasTy y t)) G Gxsyt ->
+      FillWith (CtxHasTy z (TyDot s t)) G Gzst ->
+      Gxsyt |- e \in r ->
+      Gzst |- TmLetCat t x y z e \in r
+  | TEpsR : forall G,
       G |- sink \in eps
-  | T_One_R : forall G,
+  | TOneR : forall G,
       G |- unit \in 1
-  | T_Var : forall G x s,
-      Contains (CtxHasTy x s) G ->
-      G |- (TmVar x) \in s
-  | T_SubCtx : forall G G' e s,
-      Contains G' G ->
+  | TVar : forall G x s Gxs,
+      (* fill G (CtxHasTy x s) |- (TmVar x) \in s *)
+      FillWith (CtxHasTy x s) G Gxs ->
+      Gxs |- TmVar x \in s
+  | TSubCtx : forall G G' e s,
+      CtxLEq G G' ->
       G' |- e \in s ->
       G |- e \in s
-  | T_Let : forall Gxs GD D x e e' s t,
-      FindReplace (CtxHasTy x s) D Gxs GD ->
+  | T_Let : forall G D x e e' s t Gxs GD,
+      ~fv G x ->
       D |- e \in s ->
+      (*
+      fill G (CtxHasTy x s) |- e' \in t ->
+      fill G D |- TmLet x e e' \in t
+      *)
+      FillWith (CtxHasTy x s) G Gxs ->
+      FillWith D G GD ->
       Gxs |- e' \in t ->
       GD |- TmLet x e e' \in t
+  | T_Drop : forall G x s t e Ge Gxs,
+      (*
+      fill G CtxEmpty |- e \in t ->
+      fill G (CtxHasTy x s) |- TmDrop x e \in t
+      *)
+      FillWith CtxEmpty G Ge ->
+      FillWith (CtxHasTy x s) G Gxs ->
+      Ge |- e \in t ->
+      Gxs |- drop x; e \in t
 where "G '|-' x '\in' T" := (Typed G x T).
 Hint Constructors Typed : core.
+
+(* TODO:
+Theorem typed_wf_term : forall G x T,
+  G |- x \in T ->
+  WFTerm (fv G) x.
 *)
