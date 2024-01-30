1d632008615acd9eca6b993aa4b324587894645e
Will Sturgeon <willstrgn@gmail.com>
2024-01-30 01:33:00 -0500

    Redefine `FillWith` to `Fill` for readability

--- theories/Recursion.v
+++ theories/Recursion.v
@@ -22,7 +22,7 @@ Inductive RTyped (H : hist_ctx) : context -> rec_sig -> term -> type -> Prop :=
   | TOneR : forall G E,
       RTyped H G E TmUnit TyOne
   | TVar : forall G E x s Gxs,
-      FillWith (CtxHasTy x s) G Gxs ->
+      Fill G (CtxHasTy x s) Gxs ->
       RTyped H Gxs E (TmVar x) s
   | TSub : forall G D E e s,
       Subtype G D ->
@@ -38,19 +38,19 @@ Inductive RTyped (H : hist_ctx) : context -> rec_sig -> term -> type -> Prop :=
       RTyped H (CtxSemic G D) E (TmSemic e1 e2) (TyDot s t)
   | TParL : forall G E x y z s t e r Gxsyt Gzst,
       let xs_yt := CtxComma (CtxHasTy x s) (CtxHasTy y t) in
-      FillWith xs_yt G Gxsyt ->
-      FillWith (CtxHasTy z (TyPar s t)) G Gzst ->
+      Fill G xs_yt Gxsyt ->
+      Fill G (CtxHasTy z (TyPar s t)) Gzst ->
       RTyped H Gxsyt E e r ->
       RTyped H Gzst E (TmLetPar x y z e) r
   | TCatL : forall G E x y z s t e r Gxsyt Gzst,
       let xs_yt := CtxComma (CtxHasTy x s) (CtxHasTy y t) in
-      FillWith xs_yt G Gxsyt ->
-      FillWith (CtxHasTy z (TyDot s t)) G Gzst ->
+      Fill G xs_yt Gxsyt ->
+      Fill G (CtxHasTy z (TyDot s t)) Gzst ->
       RTyped H Gxsyt E e r ->
       RTyped H Gzst E (TmLetCat t x y z e) r
   | TCut : forall G D E e1 e2 x s t Gxs GD,
-      FillWith (CtxHasTy x s) G Gxs ->
-      FillWith D G GD ->
+      Fill G (CtxHasTy x s) Gxs ->
+      Fill G D GD ->
       RTyped H D E e1 s ->
       RTyped H Gxs E e2 t ->
       RTyped H GD E (TmLet x e1 e2) t
