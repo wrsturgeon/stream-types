c8949e3132913876edcee8c80b4b59889a02eaf8
Will Sturgeon <willstrgn@gmail.com>
2024-01-29 22:27:50 -0500

    Remove `hammer` plugin and QuickChick

--- flake.nix
+++ flake.nix
@@ -7,12 +7,13 @@
       flake = false;
     };
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
-    qc-src = {
-      url = "github:quickchick/quickchick";
-      flake = false;
-    };
+    # qc-src = {
+    #   url = "github:quickchick/quickchick";
+    #   flake = false;
+    # };
   };
-  outputs = { flake-utils, hammer-src, nixpkgs, qc-src, self }:
+  # outputs = { flake-utils, hammer-src, nixpkgs, qc-src, self }:
+  outputs = { flake-utils, hammer-src, nixpkgs, self }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pname = "lambda-st";
@@ -22,17 +23,18 @@
         hammer = (import nix/hammer.nix {
           inherit coq-pkgs os-pkgs;
           src = hammer-src;
-          # }).tactics;
-        }).whole-enchilada;
-        quickchick = import nix/quickchick.nix {
-          inherit coq-pkgs;
-          src = qc-src;
-        };
+        }).tactics;
+        # quickchick = import nix/quickchick.nix {
+        #   inherit coq-pkgs;
+        #   src = qc-src;
+        # };
       in {
         packages.default = coq-pkgs.mkCoqDerivation {
           inherit pname version;
           src = ./.;
-          buildInputs = [ hammer quickchick ];
+          buildInputs = [
+            hammer # quickchick
+          ];
         };
       });
 }
