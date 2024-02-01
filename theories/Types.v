916be4dfae7e2a909e85381c034ca7b4a65243fd
Will Sturgeon <willstrgn@gmail.com>
2024-02-01 16:02:40 -0500

    . . . and some other notations

--- theories/Types.v
+++ theories/Types.v
@@ -1,7 +1,5 @@
 From QuickChick Require Import QuickChick.
 
-Declare Scope stream_type_scope.
-
 Inductive type : Set :=
   | TyEps
   | TyOne
@@ -14,11 +12,12 @@ Hint Constructors type : core.
 Derive Show for type.
 Derive Arbitrary for type.
 
-Bind Scope stream_type_scope with type.
-
 Arguments TyDot lhs%stream_type_scope rhs%stream_type_scope.
 Arguments TyPar lhs%stream_type_scope rhs%stream_type_scope.
+Arguments TyStar starred%stream_type_scope.
 
+Declare Scope stream_type_scope.
+Bind Scope stream_type_scope with type.
 Notation "'eps'" := TyEps : stream_type_scope.
 Notation "'1'" := TyOne : stream_type_scope.
 Notation "'(' lhs '.' rhs ')'" := (TyDot lhs rhs) : stream_type_scope.
