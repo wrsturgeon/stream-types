814c6fa20046e73be9116b4cdc814632eef8d6a7
Will Sturgeon <willstrgn@gmail.com>
2024-02-01 21:44:23 -0500

    Prove maximal-semantics theorem (#56)
    * Prove auxiliary maximal semantics theorem without the cut rule
    
    * ...and for cut as well
    
    * Prove the whole enchilada

--- theories/Semantics.v
+++ theories/Semantics.v
@@ -5,11 +5,11 @@ From LambdaST Require Import
   Subst
   Terms.
 
+(* Meaning of (Step n e e' p) is
+ *  "running the core term `e` on the input environment `n`
+ *   yields the output prefix `p` and steps to `e'`."
+ * Definition currently matches the one on page 9. *)
 Inductive Step : env -> term -> term -> prefix -> Prop :=
-  | SEpsR : forall n,
-      Step n TmSink TmSink PfxEpsEmp
-  | SOneR : forall n,
-      Step n TmUnit TmSink PfxOneFull
   | SVar : forall n x p,
       n x = Some p ->
       Step n (TmVar x) (TmVar x) p
@@ -17,6 +17,10 @@ Inductive Step : env -> term -> term -> prefix -> Prop :=
       Step n e1 e1' p1 ->
       Step n e2 e2' p2 ->
       Step n (e1, e2) (e1', e2') (PfxParPair p1 p2)
+  | SParL : forall n x y z e e' p1 p2 p',
+      n z = Some (PfxParPair p1 p2) ->
+      Step (env_union n (env_union (singleton_env x p1) (singleton_env y p2))) e e' p' ->
+      Step n (TmLetPar x y z e) (TmLetPar x y z e') p'
   | SCatR1 : forall n e1 e1' e2 p,
       Step n e1 e1' p ->
       ~MaximalPrefix p ->
@@ -26,10 +30,6 @@ Inductive Step : env -> term -> term -> prefix -> Prop :=
       MaximalPrefix p1 ->
       Step n e2 e2' p2 ->
       Step n (e1; e2) e2' (PfxCatBoth p1 p2)
-  | SParL : forall n x y z e e' p1 p2 p',
-      n z = Some (PfxParPair p1 p2) ->
-      Step (env_union n (env_union (singleton_env x p1) (singleton_env y p2))) e e' p' ->
-      Step n (TmLetPar x y z e) (TmLetPar x y z e') p'
   | SCatL1 : forall t n x y z e e' p p',
       n z = Some (PfxCatFst p) ->
       Step (env_union n (env_union (singleton_env x p) (singleton_env y (emp t)))) e e' p' ->
@@ -38,10 +38,14 @@ Inductive Step : env -> term -> term -> prefix -> Prop :=
       n z = Some (PfxCatBoth p1 p2) ->
       Step (env_union n (env_union (singleton_env x p1) (singleton_env y p2))) e e' p' ->
       Step n (TmLetCat t x y z e) (TmLet x (sink_tm p1) (subst_var e z y)) p'
-  | SLet : forall eta x e1 e2 e1' e2' p p',
-      Step eta e1 e1' p ->
-      Step (env_subst x p eta) e2 e2' p' ->
-      Step eta (TmLet x e1 e2) (TmLet x e1' e2') p'
+  | SEpsR : forall n,
+      Step n TmSink TmSink PfxEpsEmp
+  | SOneR : forall n,
+      Step n TmUnit TmSink PfxOneFull
+  | SCut : forall n x e1 e2 e1' e2' p p',
+      Step n e1 e1' p ->
+      Step (env_union n (singleton_env x p)) e2 e2' p' ->
+      Step n (TmLet x e1 e2) (TmLet x e1' e2') p'
   .
-(* TODO: FINISH DEFINITION *)
+Arguments Step n e e' p.
 Hint Constructors Step : core.
