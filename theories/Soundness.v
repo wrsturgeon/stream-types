e9446f913902539e9e45ed1a94be7bcc3aaddac5
Will Sturgeon <willstrgn@gmail.com>
2024-02-05 02:37:33 -0500

    Verified typechecker for property-based testing (#67)
    * Verified typechecker for property-based testing
    
    * Abort unfinished soundness proof

--- theories/Soundness.v
+++ theories/Soundness.v
@@ -19,55 +19,128 @@ From LambdaST Require Import
   Types
   Typing.
 
-Theorem soundout : forall G e s,
-  Typed G e s ->
-  forall n e' p,
-  Step n e e' p ->
+(*
+Theorem soundout : forall G,
   WFContext G ->
-  EnvTyped n G -> (
+  forall e s,
+  Typed G e s ->
+  forall n,
+  EnvTyped n G ->
+  forall e' p,
+  Step n e e' p -> (
     PrefixTyped p s /\ (
       MaximalOn (fv e) n ->
       MaximalPrefix p)).
 Proof.
-  intros G e s Ht n e' p Hs Hwf He.
+  intros G Hw e s Ht n He e' p Hs. generalize dependent G. generalize dependent s.
+  Print Step. induction Hs; intros.
+  - (* S-Var *)
+    remember (TmVar x) as vx eqn:Evx. generalize dependent n. generalize dependent x.
+    generalize dependent p. generalize dependent Hw. induction Ht; intros; sinvert Evx.
+    + destruct (maps_to_has_type _ _ _ _ _ H He) as [p' [Hp1 Hp2]].
+      rewrite H0 in Hp1. symmetry in Hp1. sinvert Hp1. split. { assumption. }
+      eapply maximal_semantics_aux. constructor. assumption.
+    + sinvert H1. eapply IHHt; clear IHHt;
+      [eapply sub_preserves_wf | reflexivity | | eapply sub_preserves_env]; eassumption.
+  - (* S-Par-R *)
+    remember (TmComma e1 e2) as e1e2 eqn:Ee1e2. generalize dependent n. generalize dependent e1.
+    generalize dependent e1'. generalize dependent e2. generalize dependent e2'.
+    generalize dependent p1. generalize dependent p2. generalize dependent Hw.
+    induction Ht; intros; sinvert Ee1e2.
+    + specialize (IHHs1 _ _ Hw Ht1 He) as [IH1p IH1m].
+      specialize (IHHs2 _ _ Hw Ht2 He) as [IH2p IH2m].
+      split. { constructor; assumption. } intro Hm. constructor; [apply IH1m | apply IH2m];
+      intros x Hx; apply Hm; [left | right]; assumption.
+    + sinvert H0. eassert (Hw' := sub_preserves_wf _ _ H Hw). specialize (IHHt Hw').
+      specialize (IHHt _ _ _ _ _ _ eq_refl _ Hs1 Hs2 IHHs1 IHHs2).
+      destruct (sub_preserves_env _ _ _ He H) as [He' Ha]. specialize (IHHt He') as [IHp IHm].
+      split; assumption.
+  - (* S-Par-L *)
+    remember (TmLetPar x y z e) as xyze eqn:Exyze. generalize dependent n. generalize dependent x.
+    generalize dependent y. generalize dependent z. generalize dependent e. generalize dependent e'.
+    generalize dependent p1. generalize dependent p2. generalize dependent p'. generalize dependent Hw.
+    induction Ht; intros; sinvert Exyze.
+    + assert (HwG := wf_ctx_hole _ Hw _ _ H3). assert (HwGxsyt : WFContext Gxsyt). {
+        clear IHHt IHHs. eapply wf_hole_iff. { eassumption. } split. { assumption. }
+        split. { repeat constructor; congruence. } split; intros Hx C; [destruct C | destruct Hx]; congruence. }
+      specialize (IHHt HwGxsyt). destruct (maps_to_has_type _ _ _ _ _ H3 He) as [p [Hp1 Hp2]].
+      rewrite H4 in Hp1. sinvert Hp1. sinvert Hp2. Search e0.
+      assert (A : exists x y z e, e0 = TmLetPar x y z e) by admit.
+      destruct A as [x' [y' [z' [e'' Exyze]]]]. clear IHHt Hs IHHs. specialize (IHHt _ _ _ _ _ _ _ _ Exyze _ H4).
+      Search e0.
+      best use: maximal_semantics, maximal_semantics_aux.
+      split. { Search p'. clear IHHt Hs IHHs. best. Search e0.
+  - (* S-Cat-R-1 *)
+  - (* S-Cat-R-2 *)
+  - (* S-Cat-L-1 *)
+  - (* S-Cat-L-2 *)
+  - (* S-Eps-R *)
+  - (* S-One-R *)
+Qed.
+*)
+
+Theorem soundness : forall G,
+  WFContext G ->
+  forall e s,
+  Typed G e s ->
+  forall n,
+  EnvTyped n G ->
+  forall e' p,
+  Step n e e' p -> (
+    PrefixTyped p s /\ (
+      MaximalOn (fv e) n ->
+      MaximalPrefix p)).
+Proof.
+  cbn. intros G Hw e s Ht n He e' p Hs.
   generalize dependent n. generalize dependent e'.
-  generalize dependent p. generalize dependent Hwf.
-  induction Ht; intros. 8: { (* Subtyping/subcontext case first *)
+  generalize dependent p. generalize dependent Hw.
+  induction Ht; intros. 9: { (* Subtyping/subcontext case first *)
     rename G' into D. (* to match the appendix *)
     destruct (sub_preserves_env _ _ _ He H) as [HD Ha].
     eapply IHHt; [eapply sub_preserves_wf | |]; eassumption. }
-  - (* T-Par-R (case 4) *)
+  - (* T-Par-R (case 4): (e1, e2) *)
     sinvert Hs. (* now we have the same assumptions as the appendix *)
-    specialize (IHHt1 Hwf _ _ _ H2 He) as [IHa1 IHa2].
-    specialize (IHHt2 Hwf _ _ _ H5 He) as [IHb1 IHb2].
+    specialize (IHHt1 Hw _ _ _ He H2) as [IHa1 IHa2].
+    specialize (IHHt2 Hw _ _ _ He H5) as [IHb1 IHb2].
     split. { constructor; assumption. } intros. cbn in *.
     constructor; [apply IHa2 | apply IHb2]; intros; apply H; [left | right]; assumption.
-  - (* T-Par-L (case 7) *)
+  - (* T-Par-L (case 7): let (x, y) = z in e *)
     sinvert Hs.
     destruct (maps_to_has_type _ _ _ _ _ H3 He) as [nz [Enz Hnz]].
     rewrite H11 in Enz. sinvert Enz. sinvert Hnz.
-    assert (HG := wf_ctx_hole _ Hwf _ _ H3).
-    assert (Hwf' : WFContext Gxsyt). {
+    assert (HG := wf_ctx_hole _ Hw _ _ H3).
+    assert (Hwxsyt : WFContext Gxsyt). {
       eapply wf_hole_iff. { eassumption. } split. { assumption. }
       split; repeat constructor; sfirstorder. }
-    specialize (IHHt Hwf' _ _ _ H12) as [Hp Hi]. { shelve. } split. { assumption. } intros.
-    eapply maximal_semantics; try eassumption. shelve.
+    specialize (IHHt Hwxsyt). edestruct IHHt as [IHp IHm]; clear IHHt; [admit | eassumption |].
+    split. { assumption. } intro Hm. apply IHm; clear IHm. intros test Htest. cbn.
+Abort.
+(*
+    destruct (eqb_spec y test). best use: maximal_semantics, maximal_semantics_aux.
+    clear H H0 H1 H2 H3 Ht Hw He H11 H12 H7 H9 HG Hwxsyt IHp Hm.
+
+    clear IHHt H12. eapply env_subctx_bind. Search EnvTyped. fail.
+    Print EnvTyped.
+    (* TODO: Problem: (EnvTyped _ Gxsyt) is really not necessarily true, not just not provable...
+     * Case on whether x & y are bound? Or require something extra a priori?
+     * Second problem: even if it were true and provable, the goal doesn't seem provable with it... *)
+    shelve.
   - (* T-Cat-R (cases 5-6) *)
-    sinvert Hwf. sinvert He. sinvert Hs.
+    sinvert Hw. sinvert He. sinvert Hs.
     + (* S-Cat-R-1 (case 5) *)
-      specialize (IHHt1 H2 _ _ _ H9 H5) as [IH1 IH2]. split. { constructor. assumption. }
+      specialize (IHHt1 H2 _ _ _ H5 H9) as [IH1 IH2]. split. { constructor. assumption. }
       intros. contradiction H12. apply IH2. cbn. intros. apply H0. left. assumption.
     + (* S-Cat-R-2 (case 6) *)
-      specialize (IHHt1 H2 _ _ _ H6 H5) as [IH1p IH1i].
-      specialize (IHHt2 H3 _ _ _ H13 H7) as [IH2p IH2i].
+      specialize (IHHt1 H2 _ _ _ H5 H6) as [IH1p IH1i].
+      specialize (IHHt2 H3 _ _ _ H7 H13) as [IH2p IH2i].
       split. { constructor; assumption. } intros. constructor; [assumption |].
       apply IH2i. intros x Hfv. apply H0. right. assumption.
   - (* T-Cat-L (cases 8-9) *)
-    assert (HG := wf_ctx_hole _ Hwf _ _ H3).
-    assert (Hwf' : WFContext Gxsyt). {
+    assert (HG := wf_ctx_hole _ Hw _ _ H3).
+    assert (Hw' : WFContext Gxsyt). {
       eapply wf_hole_iff. { eassumption. } split. { assumption. }
       split; repeat constructor; sfirstorder. }
-    specialize (IHHt Hwf').
+    specialize (IHHt Hw').
     sinvert Hs.
     + (* T-Cat-L-1 (case 8) *)
       destruct (maps_to_has_type _ _ _ _ _ H3 He) as [nz [Enz Hnz]].
@@ -92,15 +165,23 @@ Proof.
     intros. eapply maximal_semantics_aux; [| eassumption]. constructor. eassumption.
   - (* T-Let / T-Cut (case 22) *)
     sinvert Hs. eassert (HD := maps_to_hole _ _ _ _ H1 He).
-    eassert (HwfD := wf_ctx_plug _ Hwf _ _ H1).
-    specialize (IHHt1 HwfD _ _ _ H8 HD) as [IH1p IH1i].
-    assert (E1 := H1). apply reflect_fill in E1. assert (E2 := H0). apply reflect_fill in E2. subst.
-    split. Abort.
+    assert (HwD : WFContext D). { eapply wf_ctx_plug; eassumption. }
+    edestruct IHHt1 as [IH1p IH1i]; clear IHHt1; try eassumption.
+    assert (HwG : WFHole G). { eapply wf_ctx_hole; [| eassumption]. congruence. }
+    assert (HwGxs : WFContext Gxs). { eapply wf_hole_iff; repeat split; sfirstorder. }
+    edestruct IHHt2 as [IH2p IH2i]; clear IHHt2; try eassumption. { shelve. }
+    split. { assumption. } intros. eapply maximal_semantics. { eassumption. } { eassumption. }
+    cbn. intros test Hfv. destruct (eqb_spec x test). {
+      symmetry in e0. subst. eexists. repeat constructor.
+      apply IH1i. intros test Htest. apply H2. left. assumption. }
+    Abort.
+    (*
+    best time: 100 use: B1, B2, B3, B4, B5, B6, B7, B8, B9, B10, B11, (* B12, *) B13, B14, B15, B16, B17, B18, B19, B20, B21, B22, B23, B24, B25, B26, (* B27, B28, B29, *) B30, B31, B32, (* B33, *) B34, B35, B36, B37, B38, (* B39 *) B40, (* B41 *) B42, (* B43 *) B44, B45, B46, (* B47 *) B48.
+    *)
+*)
 
 (*
 (* Theorem B.50 *)
-Theorem soundness : forall G e s,
-  Typed G e s ->
   forall n e' p,
   Step n e e' p ->
   EnvTyped n G -> (
--- theories/Typing.v
+++ theories/Typing.v
@@ -1,7 +1,10 @@
-From Coq Require Import String.
+From Coq Require Import
+  Program.Wf
+  String.
 From Hammer Require Import Tactics.
 From LambdaST Require Import
   Context
+  Eqb
   FV
   Hole
   Sets
@@ -43,18 +46,20 @@ Inductive Typed : context -> term -> type -> Prop :=
   | TVar : forall G x s Gxs,
       Fill G (CtxHasTy x s) Gxs ->
       Typed Gxs (TmVar x) s
+  | TCut : forall G D x e1 e2 s t Gxs GD,
+      (* `D` is the context in which we type `e` (as in `let x := e in ...`) *)
+      ~fv G x ->
+      Typed D e1 s ->
+      Fill G (CtxHasTy x s) Gxs ->
+      Fill G D GD ->
+      Typed Gxs e2 t ->
+      Typed GD (TmLet s x e1 e2) t
   | TSubCtx : forall G G' e s,
       Subcontext G G' ->
       Typed G' e s ->
       Typed G e s
-  | TLet : forall G D x e e' s t Gxs GD,
-      ~fv G x ->
-      Typed D e s ->
-      Fill G (CtxHasTy x s) Gxs ->
-      Fill G D GD ->
-      Typed Gxs e' t ->
-      Typed GD (TmLet x e e') t
   .
+Arguments Typed G e s.
 Hint Constructors Typed : core.
 
 Theorem typing_fv : forall G e s,
@@ -76,12 +81,87 @@ Proof.
   - destruct H' as [].
   - destruct H' as [].
   - eapply fv_fill. { eassumption. } cbn. left. assumption.
-  - eapply subctx_fv_subset; [| apply IHHt]; eassumption.
   - eapply fv_fill. { eassumption. } cbn. destruct H' as [H' | [H' H'']]; [left | right]. { apply IHHt1. assumption. }
     specialize (IHHt2 _ H'). eapply fv_fill in IHHt2; [| eassumption].
     cbn in IHHt2. destruct IHHt2. { contradiction. } assumption.
+  - eapply subctx_fv_subset; [| apply IHHt]; eassumption.
 Qed.
 Hint Resolve typing_fv : core.
+
+Fixpoint typecheck (G : context) (e : term) (s : type) : bool :=
+  match e with
+    (* T-Eps-R *)
+  | TmSink =>
+      match s with TyEps => true | _ => false end
+    (* T-One-R *)
+  | TmUnit =>
+      match s with TyOne => true | _ => false end
+    (* T-Var *)
+  | TmVar x =>
+      match ctx_lookup G x with Some t => if eqb s t then true else false | None => false end
+    (* T-Par-R *)
+  | TmComma e1 e2 =>
+      match s with TyPar s t => (typecheck G e1 s && typecheck G e2 t)%bool | _ => false end
+    (* T-Cat-R *)
+  | TmSemic e1 e2 =>
+      match G, s with
+      | CtxSemic G' D', TyDot s t => (typecheck G' e1 s && typecheck D' e2 t && ctx_disj G' D')%bool
+      | _, _ => false
+      end
+    (* T-Let *)
+  | TmLet t x e1 e2 =>
+      let fve1 := fv_term_li e1 in
+      let (G, D) := zoom_in fve1 G in (
+        (negb (lcontains x (fv_hole_li G))) &&
+        typecheck D e1 t &&
+        typecheck (fill G (CtxHasTy x t)) e2 s)%bool
+  | _ =>
+      (* TODO *) false
+  end.
+
+Theorem typecheck_not_wrong : forall G,
+  WFContext G ->
+  forall e s,
+  typecheck G e s = true ->
+  Typed G e s.
+Proof.
+  intros G Hw e. generalize dependent G. induction e; cbn in *; intros.
+  - destruct s; sinvert H. constructor.
+  - destruct s; sinvert H. constructor.
+  - destruct (ctx_lookup G id) eqn:E; [| discriminate H].
+    apply ctx_lookup_fill in E as [G' EG']; [| assumption].
+    destruct (eqb_spec_type s t); sinvert H. econstructor. eassumption.
+  - destruct s; try discriminate H. apply Bool.andb_true_iff in H as [H1 H2].
+    apply IHe1 in H1; [| assumption]. apply IHe2 in H2; [| assumption]. constructor; assumption.
+  - destruct G; try discriminate H. destruct s; try discriminate H. sinvert Hw.
+    apply Bool.andb_true_iff in H as [H1 Hd]. apply Bool.andb_true_iff in H1 as [Ht1 Ht2].
+    apply IHe1 in Ht1; [| assumption]. apply IHe2 in Ht2; [| assumption]. constructor; assumption.
+  - assert (Hf := zoom_in_full G (fv_term_li e1)). destruct (zoom_in (fv_term_li e1) G) as [G' D'] eqn:E.
+    apply Bool.andb_true_iff in H as [H1 H2]. apply Bool.andb_true_iff in H1 as [Hb H1].
+    destruct (reflect_fv_hole G' bind); sinvert Hb.
+    apply IHe1 in H1; [| eapply wf_ctx_plug; eassumption]. apply IHe2 in H2. 2: {
+      eapply wf_hole_iff. { apply reflect_fill. reflexivity. } split. { eapply wf_ctx_hole; eassumption. }
+      split. { constructor. } assert (A := fv_fill _ _ _ Hf). split; congruence. }
+    econstructor; [eassumption | eassumption | | assumption | eassumption]. apply reflect_fill. reflexivity.
+  - discriminate H.
+  - discriminate H.
+Qed.
+
+Theorem typecheck_complete : forall G,
+  WFContext G ->
+  forall e s,
+  Typed G e s ->
+  typecheck G e s = true.
+Proof.
+  intros G Hw e s Ht. generalize dependent Hw. induction Ht; cbn in *; intros.
+  - rewrite IHHt1; [rewrite IHHt2 |]; [reflexivity | |]; assumption.
+  - Abort.
+
+Theorem typecheck_correct : forall G,
+  WFContext G ->
+  forall e s,
+  Bool.reflect (Typed G e s) (typecheck G e s).
+Proof. (* TODO: best use: typecheck_not_wrong, typecheck_complete *) Abort.
 
 (* TODO: add WF weakening theorem assuming all FVs are covered, then use the above to prove the below *)
 
